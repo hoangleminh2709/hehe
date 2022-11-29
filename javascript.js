5df3e79a9a789bbd8d063fea261e94b713d1ac35
hoangleminh2709 <85881055+hoangleminh2709@users.noreply.github.com>
2022-11-29 18:13:58 +0700

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -1,71 +1,103 @@
-// Q1- print to the screen even numbers from 1 to 10
-for (let i = 1; i <= 10; i++){
-    if( i % 2 == 0){
-        console.log (i);
+//  Q1. Create an object called person with name = John, age = 50.Then, access the object to alert("John is 50").
+
+person={
+  name: "John",
+  age: 50
+};
+alert(person.name + " is " + person.age);
+// we have a object:
+person={
+    name: "Jame",
+    age: 19,
+    address: "London",
+}
+
+// Q2.  add at least one property of the person object.
+person.lastName = "William";
+console.log (person)
+// Q3. Please update value of address into "English"
+person.address ="English"
+console.log (person)
+// Q4. remove at least one property of the person object.
+delete person.age 
+console.log (person)
+// q5 Let's create an object containing the function inside and then call the function via object
+{const person = {
+  name: "Jullia",
+  age: 20,
+  address: "London",
+  ask: function () {
+    console.log ('How old is she?');
+  },
+  fullname: function () {
+    console.log (this.name + " is " + this.age);
+  }
+};
+person.ask();
+person.fullname ();
+}
+
+
+//  we have a object:
+const teacher = {
+    firstName : "Nam",
+    lastName: "Nguyen",
+    subject: ['Math', 'English','Music']
+};
+// Q6 Use a for of loop and console the value of the subject attribute to the screen.
+for (let item of teacher.subject) {
+  console.log (item);
+}
+//  we have a:
+ const arrays = [
+    {
+        id: 1,
+        name: "Nguyễn Văn An",
+        age: 22,
+      },
+      {
+        id: 2,
+        name: "Nguyễn Lan Anh",
+        age: 21,
+      },
+      {
+        id: 3,
+        name: "Hồ Văn Trí",
+        age: 23,
+      },
+      {
+        id: 4,
+        name: "Cao Văn La",
+        age: 24,
+      },
+    ];
+    console.log (arrays)
+    for (let person of arrays)
+    {
+      console.log (`ID:${person.id}, Name: ${person.name}, Age: ${person.age}`)
     }
-}
-
-// Q2 - using if else  and while to Check prime number.
-// input: 5
-// output: True
-var n = (5)
-let isPrime = true;
-if (n === 1) {
-    console.log(false);
-}
-
-else if (n > 1) {
-    for (let i = 2; i < n; i++) {
-        if (n % i == 0) {
-            isPrime = false;
-            break;
-        }
-    }
-
-    if (isPrime) {
-        console.log(true);
-    } else {
-        console.log(false);
-    }
-}
-
-else {
-    console.log(false);
-}
+    // Combine the attributes learned in the previous lesson .
+    // Q 7: Add a new user to the beginning of the array (using unshift())
+ 
+  const newuser = arrays.unshift({id: 0, name: "Nguyễn Trí Thành", age: 26});
+  console.log (arrays)
+  
+   
+    // Q8: Add a new user at the end of the array (using push())
+    const newuser2 = arrays.push({id: 5, name: "Nguyễn Trí Toàn", age: 29});
+    console.log (arrays)
     
-
-
-// Q3-sort the following array in ascending order using loop
-// input: [2,1,7,2,6,9,1,99,12,56]
-// output:[1,1,2,2,6,7,9,12,56,99]
-var myArray = [2, 1, 7, 2, 6, 9, 1, 99, 12, 56];
-
-for (var i = 1; i < myArray.length; i++)
-    for (var j = 0; j < i; j++)
-        if (myArray[i] < myArray[j]) {
-          var x = myArray[i];
-          myArray[i] = myArray[j];
-          myArray[j] = x;
-        }
-
-console.log(myArray);
-
-
-// Q4 - sort the following array in descending order using loop
-// input: [2,1,7,2,6,9,1,99,12,56]
-// output:[99,56,12,9,7,6,2,2,1,1]
-var myArray = [2, 1, 7, 2, 6, 9, 1, 99, 12, 56];
-
-for (var a = 1; a < myArray.length; a++)
-    for (var b = 0; b < a; b++)
-        if (myArray[a] > myArray[b]) {
-          var z = myArray[a];
-          myArray[a] = myArray[b];
-          myArray[b] = z;
-        }
-
-
-// Q5-  Given a string is the path of a file in the computer. The correct path is a path that has no leading and trailing slashes (/) and only one slash in between folders to separate it. Please remove the extra slashes to get a correct path
-// input:  ////laptrinh//////code//////javascript/////
-// output: laptrinh/code/javascript
-
+    // Q9: add multiple users to the array  (using push())
+    const newusers = arrays.push({id: 6, name: "Nguyễn Cung", age: 30},{id: 7, name: "Nguyễn Thị Trang", age: 24});
+    console.log (arrays)
+    
+    // Q10: remove 1 user from the array (using(pop))
+    const deleteuser = arrays.pop({id: 7, name: "Nguyễn Thị Trang", age: 24});
+    console.log (arrays)
+    
+    // Q11: filter out users with age >= 22;( using(filter())
+ 
+    // extend part:
+    // Q12: use for loop to get users with age >= 23
+    
+    // Q13: write an object using setter & getter to add new value and get value. key word (Getter, setter)
